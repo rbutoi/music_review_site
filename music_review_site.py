cb115f0b5125a6a2d41a8c61d29ddf17cb11e973
marcokstephen <marcok.stephen@gmail.com>
2014-09-20 11:33:11 -0400

    Additions to communities page

--- music_review_site.py
+++ music_review_site.py
@@ -4,16 +4,24 @@ from google.appengine.api import users
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+	self.response.write('<html><body>Welcome to music_review_site. <a href="/communities">Go to communities</a></body></html>')
+
+class CommunityPage(webapp2.RequestHandler):
+    def get(self):
 	user = users.get_current_user()
 
 	if user:
 		MAIN_STRING = ('Welcome, %s! (<a href="%s">Click here to logout</a>)' %
 			(user.nickname(), users.create_logout_url('/')))
-		self.response.write('<html><body>%s</body></html>' % MAIN_STRING)
+		self.response.write('<html><body>%s' % MAIN_STRING)
+		self.response.write('<br /><a href="/create">Create a community</a>')
+		self.response.write('<br /><a href="/join">Join a community</a>')
+		self.response.write('<br /><hr>Your currently joined communities:')
+		self.response.write('</body></html>')
 	
 	else:
 		self.redirect(users.create_login_url(self.request.uri))
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage)
+    ('/', MainPage),('/communities',CommunityPage)
 ], debug=True)
