301912666f5221714b3f5b24c0f5c928e46e8523
marcokstephen <marcok.stephen@gmail.com>
2014-09-20 10:40:08 -0400

    Creating a login/logout function for the communities page

--- music_review_site.py
+++ music_review_site.py
@@ -2,27 +2,18 @@ import webapp2
 import cgi
 from google.appengine.api import users
 
-MAIN_STRING = """\
-<html>
-  <body>
-     <form action="/sign" method="post">
-	<div><textarea name="content" rows="3" cols="60"></textarea></div>
-	<div><input type="submit" value="Sign guestbook!"></div>
-     </form>
-  </body>
-</html>
-"""
-
 class MainPage(webapp2.RequestHandler):
     def get(self):
-	self.response.write(MAIN_STRING)
+	user = users.get_current_user()
 
-class TestPage(webapp2.RequestHandler):
-    def post(self):
-        self.response.write('<html><body>You wrote: <pre>')
-	self.response.write(cgi.escape(self.request.get('content')))
-	self.response.write('</pre></body></html>')
+	if user:
+		MAIN_STRING = ('Welcome, %s! (<a href="%s">Click here to logout</a>)' %
+			(user.nickname(), users.create_logout_url('/')))
+		self.response.write('<html><body>%s</body></html>' % MAIN_STRING)
+	
+	else:
+		self.redirect(users.create_login_url(self.request.uri))
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage),('/sign', TestPage),
+    ('/', MainPage)
 ], debug=True)
