5e422259692e8570612dd9812d0ad615975d005e
Radu Butoi <rbutoi@gmail.com>
2014-09-20 16:04:16 -0400

    community page

--- community_page.py
+++ community_page.py
@@ -1,20 +1,13 @@
-import webapp2
-import cgi
 from google.appengine.api import users
 from base_handler import *
 
 class CommunityPage(BaseHandler):
     def get(self):
-	user = users.get_current_user()
+        user = users.get_current_user()
 
 	if user:
-		MAIN_STRING = ('Welcome, %s! (<a href="%s">Click here to logout</a>)' %
-			(user.nickname(), users.create_logout_url('/')))
-		self.response.write('<html><body>%s' % MAIN_STRING)
-		self.response.write('<br /><a href="/create">Create a community</a>')
-		self.response.write('<br /><a href="/join">Join a community</a>')
-		self.response.write('<br /><hr>Your currently joined communities:')
-		self.response.write('</body></html>')
-	
+            # MAIN_STRING = ('Welcome, %s! (<a href="%s">Click here to logout</a>)' %
+            # 	(user.nickname(), users.create_logout_url('/')))
+            self.render_response('community_page.html')
 	else:
-		self.redirect(users.create_login_url(self.request.uri))
+            self.redirect(users.create_login_url(self.request.uri))
