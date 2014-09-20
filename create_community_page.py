c89e087ff36ea144e1b93c1c28353fa02bf88e16
marcokstephen <marcok.stephen@gmail.com>
2014-09-20 17:27:44 -0400

    Inserting into database has errors for create_community_page. About to remove password requirement for logging in

--- create_community_page.py
+++ create_community_page.py
@@ -2,6 +2,8 @@ from base_handler import *
 import cgi
 import os
 import MySQLdb
+from google.appengine.api import users;
+
 
 class CreateCommunityPage(BaseHandler):
     def get(self):
@@ -15,3 +17,13 @@ class CreateCommunityPage(BaseHandler):
 	    db = MySQLdb.connect(unix_socket='/cloudsql/your-project-id:your-instance-name', user='root')
 	else:
             db = MySQLdb.connect(host='localhost', user='root', passwd="htndjango",db="musicsite")
+
+	cursor = db.cursor()
+	result = cursor.execute('SELECT * FROM communities WHERE name = ' + name)
+	count = result.rowcount
+	if (rowcount == 0):
+		#add the new community
+		cursor.execute('INSERT INTO communities (name, pin_password) VALUES ('+name+','+password+');')
+	else:
+		#that community name already exists!
+		self.response.write("That community aready exists!")
