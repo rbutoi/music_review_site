1e26e9e18a79038de3d3e083bc439b3ab2fb3476
marcokstephen <marcok.stephen@gmail.com>
2014-09-20 18:13:02 -0400

    Finish creating community in database

--- create_community_page.py
+++ create_community_page.py
@@ -10,8 +10,9 @@ class CreateCommunityPage(BaseHandler):
         self.render_response('create_community_page.html')
 
     def post(self):
-	password = cgi.escape(self.request.get('password'))
 	name = cgi.escape(self.request.get('name'))
+	user = users.get_current_user()
+	email = user.email()
 
 	if (os.getenv('SERVER_SOFTWARE') and os.getenv('SERVER_SOFTWARE').startswith('Google App Engine/')):
 	    db = MySQLdb.connect(unix_socket='/cloudsql/your-project-id:your-instance-name', user='root')
@@ -19,11 +20,18 @@ class CreateCommunityPage(BaseHandler):
             db = MySQLdb.connect(host='localhost', user='root', passwd="htndjango",db="musicsite")
 
 	cursor = db.cursor()
-	result = cursor.execute('SELECT * FROM communities WHERE name = ' + name)
-	count = result.rowcount
-	if (rowcount == 0):
+	result = cursor.execute('SELECT * FROM communities WHERE name = "%s";' % name)
+	count = cursor.rowcount
+
+	if (count == 0):
 		#add the new community
-		cursor.execute('INSERT INTO communities (name, pin_password) VALUES ('+name+','+password+');')
+		stringToExecute = 'INSERT INTO communities (name) VALUES ("%s");' % name		
+		cursor.execute(stringToExecute)
+		stringToExecute = 'INSERT INTO users (community_name, email, invite_accepted) VALUES ("%s","%s",%s);' % (name, email, "false")
+		cursor.execute(stringToExecute)
+		db.commit()
 	else:
 		#that community name already exists!
 		self.response.write("That community aready exists!")
+
+	db.close()
