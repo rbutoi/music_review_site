9792e814fa65bb3c04bb2f34724adeb058afdcdd
marcokstephen <marcok.stephen@gmail.com>
2014-09-20 20:18:59 -0400

    Completed joining community page functionality

--- join_community_page.py
+++ join_community_page.py
@@ -31,6 +31,38 @@ class JoinCommunityPage(BaseHandler):
 			"message": "You have %s pending invite(s)!" % count,
 			"invites": pendinglist
 		}
+	db.close()
 
         self.render_response('join_community_page.html', **template_messages)
-	
+
+    def post(self):
+	community = cgi.escape(self.request.get('community'))
+	user = users.get_current_user()
+	email = user.email()
+
+	if (os.getenv('SERVER_SOFTWARE') and os.getenv('SERVER_SOFTWARE').startswith('Google App Engine/')):
+		db = MySQLdb.connect(unix_socket='/cloudsql/your-project-id:your-instance-name', user='root')
+	else:
+		db = MySQLdb.connect(host='localhost', user='root', passwd="htndjango",db="musicsite")
+
+	cursor = db.cursor()
+	result = cursor.execute('UPDATE users SET invite_accepted=1 WHERE email="%s" AND community_name="%s";' % (email,community))
+	db.commit()
+
+	result = cursor.execute('SELECT community_name FROM users WHERE email = "%s" AND invite_accepted = 0;' % email)
+	count = cursor.rowcount
+	if (count == 0):
+		template_messages={
+			"message": "%s has been joined successfully! You have no pending invites!" % community
+		}
+	else:
+		pendinglist = []
+		for row in cursor:
+			pendinglist.append(row[0])
+		invitelist = ""
+		template_messages={
+			"message": "%s has been joined successfully! You have %s pending invite(s)!" % (community,count),
+			"invites": pendinglist
+		}
+	db.close()
+        self.render_response('join_community_page.html', **template_messages)
