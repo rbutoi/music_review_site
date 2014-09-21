548e9119ecd21a919d07c9a0dff01f812c9d546d
marcokstephen <marcok.stephen@gmail.com>
2014-09-21 00:40:33 -0400

    fixing exploits

--- join_community_page.py
+++ join_community_page.py
@@ -7,62 +7,68 @@ import os
 class JoinCommunityPage(BaseHandler):
     def get(self):
 	user = users.get_current_user()
-	email = user.email()
-
-	if (os.getenv('SERVER_SOFTWARE') and os.getenv('SERVER_SOFTWARE').startswith('Google App Engine/')):
-		db = MySQLdb.connect(unix_socket='/cloudsql/hack-the-north-1:its-not-django', db='musicsite', user='root')
+	if not user:
+		self.redirect('/')
 	else:
-		db = MySQLdb.connect(host='localhost', user='root', passwd="htndjango",db="musicsite")
+		email = user.email()
 
-	cursor = db.cursor()
-	result = cursor.execute('SELECT community_name,community_id FROM users WHERE email = "%s" AND invite_accepted = 0 AND invite_hidden=0;' % email)
-	count = cursor.rowcount
+		if (os.getenv('SERVER_SOFTWARE') and os.getenv('SERVER_SOFTWARE').startswith('Google App Engine/')):
+			db = MySQLdb.connect(unix_socket='/cloudsql/hack-the-north-1:its-not-django', db='musicsite', user='root')
+		else:
+			db = MySQLdb.connect(host='localhost', user='root', passwd="htndjango",db="musicsite")
 
-	if (count == 0):
-		template_messages={
-			"message": "You have no pending invites!"
-		}
-	else:
-		#known bug where if you have two pending invites of the same name, accepting will cause you to accept both
-		pendinglist = []
-		for row in cursor:
-			pendinglist.append(row[0])
-		template_messages={
-			"message": "You have %s pending invite(s)!" % count,
-			"invites": pendinglist
-		}
-	db.close()
+		cursor = db.cursor()
+		result = cursor.execute('SELECT community_name,community_id FROM users WHERE email = "%s" AND invite_accepted = 0 AND invite_hidden=0;' % email)
+		count = cursor.rowcount
 
-        self.render_response('join_community_page.html', **template_messages)
+		if (count == 0):
+			template_messages={
+				"message": "You have no pending invites!"
+			}
+		else:
+			#known bug where if you have two pending invites of the same name, accepting will cause you to accept both
+			pendinglist = []
+			for row in cursor:
+				pendinglist.append(row[0])
+			template_messages={
+				"message": "You have %s pending invite(s)!" % count,
+				"invites": pendinglist
+			}
+		db.close()
+
+		self.render_response('join_community_page.html', **template_messages)
 
     def post(self):
 	community = cgi.escape(self.request.get('community'))
 	user = users.get_current_user()
-	email = user.email()
-
-	if (os.getenv('SERVER_SOFTWARE') and os.getenv('SERVER_SOFTWARE').startswith('Google App Engine/')):
-		db = MySQLdb.connect(unix_socket='/cloudsql/hack-the-north-1:its-not-django', db='musicsite', user='root')
+	if not user:
+		self.redirect('/')
 	else:
-		db = MySQLdb.connect(host='localhost', user='root', passwd="htndjango",db="musicsite")
+		email = user.email()
 
-	cursor = db.cursor()
-	result = cursor.execute('UPDATE users SET invite_accepted=1 WHERE email="%s" AND community_name="%s";' % (email,community))
-	db.commit()
+		if (os.getenv('SERVER_SOFTWARE') and os.getenv('SERVER_SOFTWARE').startswith('Google App Engine/')):
+			db = MySQLdb.connect(unix_socket='/cloudsql/hack-the-north-1:its-not-django', db='musicsite', user='root')
+		else:
+			db = MySQLdb.connect(host='localhost', user='root', passwd="htndjango",db="musicsite")
 
-	result = cursor.execute('SELECT community_name,community_id FROM users WHERE email = "%s" AND invite_accepted = 0 AND invite_hidden=0;' % email)
-	count = cursor.rowcount
-	if (count == 0):
-		template_messages={
-			"message": "%s has been joined successfully! You have no pending invites!" % community
-		}
-	else:
-		pendinglist = []
-		for row in cursor:
-			pendinglist.append(row[0])
-		invitelist = ""
-		template_messages={
-			"message": "%s has been joined successfully! You have %s pending invite(s)!" % (community,count),
-			"invites": pendinglist
-		}
-	db.close()
-        self.render_response('join_community_page.html', **template_messages)
+		cursor = db.cursor()
+		result = cursor.execute('UPDATE users SET invite_accepted=1 WHERE email="%s" AND community_name="%s";' % (email,community))
+		db.commit()
+
+		result = cursor.execute('SELECT community_name,community_id FROM users WHERE email = "%s" AND invite_accepted = 0 AND invite_hidden=0;' % email)
+		count = cursor.rowcount
+		if (count == 0):
+			template_messages={
+				"message": "%s has been joined successfully! You have no pending invites!" % community
+			}
+		else:
+			pendinglist = []
+			for row in cursor:
+				pendinglist.append(row[0])
+			invitelist = ""
+			template_messages={
+				"message": "%s has been joined successfully! You have %s pending invite(s)!" % (community,count),
+				"invites": pendinglist
+			}
+		db.close()
+		self.render_response('join_community_page.html', **template_messages)
