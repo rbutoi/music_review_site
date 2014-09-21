32b2db526d4d5d68e9181100f7badd252b85f146
marcokstephen <marcok.stephen@gmail.com>
2014-09-21 10:04:21 -0400

    Leaving a community now works

--- manage_users.py
+++ manage_users.py
@@ -3,6 +3,7 @@ import cgi
 import MySQLdb
 from google.appengine.api import users
 import os
+import time
 
 class ManageUsers(BaseHandler):
     def get(self,id):
@@ -11,7 +12,8 @@ class ManageUsers(BaseHandler):
 	else:
 	    db = MySQLdb.connect(host='localhost', user='root', passwd="htndjango",db="musicsite")
 
-
+	self.response.write('<form action="" method="post"><input type="hidden" name="leaveboolean" value="1"><input type="submit" value="Leave community"></form>')
+	
 	cursor = db.cursor()
 	cursor.execute('SELECT name FROM communities WHERE id = %s'%id)
 
@@ -30,19 +32,29 @@ class ManageUsers(BaseHandler):
 	else:
 	    db = MySQLdb.connect(host='localhost', user='root', passwd="htndjango",db="musicsite")
 
+	leavestatus = cgi.escape(self.request.get('leaveboolean'))
 	community_id = cgi.escape(self.request.get('id'))
-	email = cgi.escape(self.request.get('email'))
-	self.response.write('%s has been invited!<br><br>' % email)
+	if (leavestatus == "1"):
+		user = users.get_current_user()
+		email = user.email()
+		self.response.write("You have left this community.<br><a href='/communities'>Return to Communities</a>")
+		cursor = db.cursor()
+		cursor.execute('UPDATE users SET invite_hidden=1, invite_accepted=0 WHERE community_id = %s AND email = "%s"' % (id,email))
+		db.commit()
+	else:
+		self.response.write('<form action="" method="post"><input type="hidden" name="leaveboolean" value="1"><input type="submit" value="Leave community"></form>')
+		email = cgi.escape(self.request.get('email'))
+		self.response.write('%s has been invited!<br><br>' % email)
 
-	cursor = db.cursor()
-	cursor.execute('SELECT name FROM communities WHERE id = %s'%id)
-	community_name = ""
-	for row in cursor:
-		community_name = row[0]
-	cursor.execute('INSERT INTO users VALUES (%s,"%s","%s","%s",0,0)'%(id,community_name,email,email))
-	db.commit()
-	self.response.write('Add new user email: <form action="" method="post"><input type="hidden" name="community_name" value="%s"><input type="text" name="email"><input type="submit" value="Add User"></form>'%community_name)
-	cursor.execute('SELECT email FROM users WHERE community_id = %s'%id)
-	self.response.write("Current members in %s:"%community_name)
-	for row in cursor:
-	    self.response.write('<br>%s' % (row[0]))
+		cursor = db.cursor()
+		cursor.execute('SELECT name FROM communities WHERE id = %s'%id)
+		community_name = ""
+		for row in cursor:
+			community_name = row[0]
+		cursor.execute('INSERT INTO users VALUES (%s,"%s","%s","%s",0,0)'%(id,community_name,email,email))
+		db.commit()
+		self.response.write('Add new user email: <form action="" method="post"><input type="hidden" name="community_name" value="%s"><input type="text" name="email"><input type="submit" value="Add User"></form>'%community_name)
+		cursor.execute('SELECT email FROM users WHERE community_id = %s'%id)
+		self.response.write("Current members in %s:"%community_name)
+		for row in cursor:
+		    self.response.write('<br>%s' % (row[0]))
