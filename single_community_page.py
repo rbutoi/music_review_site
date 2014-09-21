5a865a996f4e72ee93c54f117f031c5f0c96f61a
marcokstephen <marcok.stephen@gmail.com>
2014-09-21 10:31:51 -0400

    Fixing bugs on deployment version

--- single_community_page.py
+++ single_community_page.py
@@ -6,11 +6,10 @@ import MySQLdb
 class SingleCommunityPage(BaseHandler):
     def get(self, community):
         user = users.get_current_user()
-	email = user.email()
-
 	if user:
+	    email = user.email()
             if (os.getenv('SERVER_SOFTWARE') and os.getenv('SERVER_SOFTWARE').startswith('Google App Engine/')):
-                db = MySQLdb.connect(unix_socket='/cloudsql/hack-the-north-1:its-not-django', user='root')
+                db = MySQLdb.connect(unix_socket='/cloudsql/hack-the-north-1:its-not-django',db='musicsite', user='root')
             else:
                 db = MySQLdb.connect(host='localhost', user='root', passwd="htndjango",db="musicsite")
 
@@ -21,3 +20,5 @@ class SingleCommunityPage(BaseHandler):
 	    else:
 		#show the page
 		self.response.write("<a href='/manage/%s'><button>Manage this community</button></a>"%community)
+	else:
+	    self.redirect('/communities')
